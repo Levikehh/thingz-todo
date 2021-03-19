bd1f037bba44c35d75ae8138fd99059792534cd2
Dóczi Levente <bkalevi@gmail.com>
2021-03-19 10:35:59 +0100

    Final version

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
-/* import {
-	BrowserRouter as Router,
-	Switch,
-	Route
-} from "react-router-dom"; */
-
-// Components
-/* import Navbar from './components/navbar' */
 
 // Pages
 import Home from './pages/home'
-/* import Signin from './pages/signin' */
 
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
 	render() {
 		return (
-			<Home />
-			{/* <Router>
-				<Switch>
-					<Route path='/' exact render={() => <Home />} />
-					<Route path='/signin' exact render={() => <Signin />} />
-				</Switch>
-			</Router> */}
+			<>
+				<Home />
+			</>
 		);
 	}
 }
-
-export default App;
